57c188e1dad7146b4a050d17dca64de34b68678a
Kevin Xu <kevinxu8099@gmail.com>
2020-07-14 20:27:55 -0700

    Added Carousel

--- SC2Django/PlayerMatch/views.py
+++ SC2Django/PlayerMatch/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import FormView
 from .forms import FileFieldForm
 from .tasks import process_uploaded_replay, selenium_process_replay
 from django.urls import reverse
-from .models import OverallSentiment
+from .models import OverallSentiment, PlayerMatchSingular
 # Create your views here.
 
 
@@ -14,6 +14,18 @@ class FileFieldView(FormView):
     def get_context_data(self, **kwargs):
         context = super(FileFieldView, self).get_context_data(**kwargs)
         curSentiments = OverallSentiment.objects.get(pk=1)
+        allPlayers = PlayerMatchSingular.objects.all()
+
+        negativeMessages = []
+        for i in allPlayers:
+            if len(negativeMessages) >= 100:
+                break
+            for num in range(0, len(i.messages)):
+                if i.messageSentiments[num] < -0.5:
+                    negativeMessages.append('"' + i.messages[num] + '"     ' + "       -" + i.username)
+                    break
+
+        context['curMessages'] = negativeMessages
 
         context['terran'] = str(round((curSentiments.terranSentimentOverall/curSentiments.terranSentimentCount)*100, 2))
 
